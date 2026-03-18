97fdcee09b374fd18bd9baa871f5963599ef6162
Nandhinisundara <nandhinisundara25@gmail.com>
2026-03-18 15:20:58 +0300

    Add files via upload
    DAY1 ASSIGNMENT COMPLETED

---
+++ HomeAssignmentDAY1.js
@@ -0,0 +1,35 @@
+//Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`, 
+//including an uninitialized variable.
+
+// step 1 : let keyword
+//step 2: initialization and uninitialization going to use
+
+//STRING 
+// Initialization value with variable
+let Username="Nandhini"
+console.log(Username);
+console.log(typeof Username);
+
+//uninitialized 
+let  organization
+console.log(typeof organization);
+
+//NUMBER
+let Number=9698668435
+console.log(Number);
+console.log(typeof Number);
+
+//Undefined
+
+let useplaywright
+console.log(useplaywright);
+console.log(typeof useplaywright);
+
+//Boolean
+
+let knowAutomation= true
+console.log(knowAutomation);
+console.log(typeof knowAutomation);
+
+
+
